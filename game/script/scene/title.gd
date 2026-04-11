86cf39d33b3f452a8a6081dec0d28793744b779b
pupungeggang6 <pupungeggang6@gmail.com>
2026-04-11 16:02:20 +0900

    Start

---
+++ game/script/scene/title.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+func _ready() -> void:
+    pass
+
+func _process(delta: float) -> void:
+    pass
+    
+func _on_button_start_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+            get_tree().change_scene_to_file("res://scene/characterselect.tscn")
+
+func _on_button_quit_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+            get_tree().quit()
