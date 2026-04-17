c01fcfe6e11ff331cdb3b2196b49f5b0050c474d
pupungeggang6 <pupungeggang6@gmail.com>
2026-04-17 14:39:53 +0900

    Character select page WIP

--- game/script/scene/characterselect.gd
+++ game/script/scene/characterselect.gd
@@ -1,12 +1,28 @@
 extends Node2D
 
+var selected_character: int = -1
+
 func _ready() -> void:
-    pass # Replace with function body.
+    var character_buttons = get_node("UI/CharacterButtons")
+    for i in range(6):
+        var row: int = int(i / 3)
+        var col: int = i % 3
+        var button = load("res://scene/uielement/characterbutton.tscn").instantiate()
+        button.id = i
+        button.position = Vector2(20 + 140 * col, 120 + 140 * row)
+        button.size = Vector2(120, 120)
+        button.button_up.connect(on_character_button_click.bind(button))
+        character_buttons.add_child(button)
 
 func _process(delta: float) -> void:
     pass
 
-func _on_button_back_gui_input(event: InputEvent) -> void:
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            get_tree().change_scene_to_file("res://scene/title.tscn")
+func _on_button_back_button_up() -> void:
+    get_tree().change_scene_to_file("res://scene/title.tscn")
+    
+func _on_button_start_pressed() -> void:
+    if selected_character != -1:
+        get_tree().change_scene_to_file("res://scene/ready.tscn")
+
+func on_character_button_click(button: Node) -> void:
+    selected_character = button.id
--- game/script/scene/title.gd
+++ game/script/scene/title.gd
@@ -6,17 +6,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     pass
     
-func _on_button_start_gui_input(event: InputEvent) -> void:
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            get_tree().change_scene_to_file("res://scene/characterselect.tscn")
+func _on_button_start_button_up() -> void:
+    get_tree().change_scene_to_file("res://scene/characterselect.tscn")
 
-func _on_button_collection_gui_input(event: InputEvent) -> void:
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            get_tree().change_scene_to_file("res://scene/collection.tscn")
+func _on_button_collection_button_up() -> void:
+    get_tree().change_scene_to_file("res://scene/collection.tscn")
 
-func _on_button_quit_gui_input(event: InputEvent) -> void:
-    if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            get_tree().quit()
+func _on_button_quit_button_up() -> void:
+    get_tree().quit()
